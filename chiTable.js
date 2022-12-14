f1af2370c656cb8e13382ff31f9ad1c43b013be9
Jacob Showman <jacob.showman@mrjaffesclass.com>
2022-12-14 13:17:59 -0800

    Calculates percent occurrence of letters in sample text. Calculates chi for English, but not other languages, because of a divide by zero error.

---
+++ chiTable.js
@@ -0,0 +1,19 @@
+chiTable = {
+    "English" : 0,
+    "French" : 0,
+    "German" : 0,
+    "Spanish" : 0,
+    "Portugese" : 0,
+    "Esperanto" : 0,
+    "Italian" : 0,
+    "Turkish" : 0,
+    "Swedish" : 0,
+    "Polish" : 0,
+    "Dutch" : 0,
+    "Danish" : 0,
+    "Icelandic" : 0,
+    "Finnish" : 0,
+    "Czech" : 0
+}
+
+module.exports = chiTable;
